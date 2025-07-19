f6590a5a4d83b78824666a59bcd79f7d6926a3f2
lora_y <lora@yelcick.org>
2025-07-19 13:51:21 -0400

    Validated and displayed inputs

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-//unordered list where the player's guessed letters will appear
-const guessedLetters = document.querySelector("guessed-letters");
-//button with Guess
+
+const guessedLettersDisplay = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
-//guessed letter box
 const letterInput = document.querySelector("#letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-//remaining number of guesses
 const remainingGuesses = document.querySelector(".remaining");
-//empty paragraph where messages will appear when the player guesses a letter
+const remaingGuessesSpan = document.querySelector(".remaining span")
 const message = document.querySelector(".message");
-//hidden button that will appear prompting hte player to play again
-const playAgain = document.querySelector(".play-again");
+const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const updateWordInProgress = function (word) {
     const placeholders = [];
@@ -25,10 +22,39 @@ const updateWordInProgress = function (word) {
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const currentGuessedLetter = letterInput.value;
-    console.log(currentGuessedLetter);
+    const currentGuess = letterInput.value;
+    const validGuess = inputValidation(currentGuess);
+    if (validGuess) {
+        makeGuess(currentGuess)
+    };
     letterInput.value = "";
-
 });
+
+const inputValidation = function (currentGuess) {
+    const acceptedLetter = /[a-zA-Z]/;
+    message.innerHTML = "";
+    if (currentGuess === "") {
+        message.innerHTML = "Type AT LEAST ONE letter.";
+    } else if (currentGuess.length >= 2) {
+        message.innerHTML = "Type ONLY ONE letter.";
+    } else if (!(currentGuess.match(acceptedLetter))) {
+        message.innerHTML = "Type one LETTER.";
+    } else {
+        return currentGuess;
+    }
+};
+
+const makeGuess = function (currentGuess) {
+    currentGuess = currentGuess.toUpperCase();
+    if (guessedLetters.includes(currentGuess)) {
+        message.innerHTML = `You already guessed ${currentGuess}`;
+    } else {
+        guessedLetters.push(currentGuess);
+        let listItem = document.createElement("li");
+        listItem.innerText = currentGuess;
+        guessedLettersDisplay.append(listItem);
+    }
+};
+
 
 updateWordInProgress(word);
